8f2893518880fcdc10bdacc330b44ef6245eb66d
cevantime <cevantime@cevantime-N56VZ>
2015-08-28 18:42:08 +0200

    - amélioration du système d'installation de module

--- bo/0.0.1/views/layout/bo.php
+++ bo/0.0.1/views/layout/bo.php
@@ -274,7 +274,7 @@ and open the template in the editor.
 		</div>
 
 
-		<script src="<?php echo base_url('lib/bootstrap/js/bootstrap.js') ?>"></script>
+		<script src="<?php echo base_url('js/bo/bootstrap/js/bootstrap.js') ?>"></script>
 		<script type="text/javascript">
 					$("[rel=tooltip]").tooltip();
 					$(function () {
