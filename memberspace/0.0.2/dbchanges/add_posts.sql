880529b4222c737d9af9a28bafed86b41ba3280a
Cevantime <thibaulttruffert@hotmail.com>
2015-11-06 22:21:16 +0100

    - ajout (incomplet) des règles de validation da la validation avancée

---
+++ memberspace/0.0.2/dbchanges/add_posts.sql
@@ -0,0 +1,21 @@
+--
+-- Structure de la table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
