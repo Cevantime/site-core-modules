92575dc3c57cd80b75c5dd642f36d515cd3060f4
Cevantime <thibaulttruffert@hotmail.com>
2015-11-16 20:35:30 +0100

    - correction bug flashmessage (3)

--- flashmessages/0.0.1/helpers/flashmessages_helper.php
+++ flashmessages/0.0.1/helpers/flashmessages_helper.php
@@ -4,7 +4,7 @@ if(!function_exists('add_message')) {
 	
 	function add_message($message, $type=null) {
 		$CI =& get_instance();
-		$CI->load->library('flashmessages/flashmessagesmanager');
+		$CI->load->library('flashmessages/flashMessagesManager');
 		$CI->flashmessagesmanager->pushNewMessage($message, $type);
 	}
 	
@@ -14,7 +14,7 @@ if(!function_exists('add_success')) {
 	
 	function add_success($message) {
 		$CI =& get_instance();
-		$CI->load->library('flashmessages/flashmessagesmanager');
+		$CI->load->library('flashmessages/flashMessagesManager');
 		$CI->flashmessagesmanager->pushNewMessage($message, FlashMessage::TYPE_SUCCESS);
 	}
 	
@@ -23,7 +23,7 @@ if(!function_exists('add_error')) {
 	
 	function add_error($message) {
 		$CI =& get_instance();
-		$CI->load->library('flashmessages/flashmessagesmanager');
+		$CI->load->library('flashmessages/flashMessagesManager');
 		$CI->flashmessagesmanager->pushNewMessage($message, FlashMessage::TYPE_ERROR);
 	}
 	
@@ -32,7 +32,7 @@ if(!function_exists('add_warning')) {
 	
 	function add_warning($message) {
 		$CI =& get_instance();
-		$CI->load->library('flashmessages/flashmessagesmanager');
+		$CI->load->library('flashmessages/flashMessagesManager');
 		$CI->flashmessagesmanager->pushNewMessage($message, FlashMessage::TYPE_WARNING);
 	}
 	
