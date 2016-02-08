229956879feecb89b15c62fceda8964534d9c198
Cevantime <thibaulttruffert@hotmail.com>
2016-02-08 12:53:07 +0100

    - amélioration design visu/print expedition (2)

--- bo/0.0.2/dbchanges/init.sql
+++ bo/0.0.2/dbchanges/init.sql
@@ -1,3 +1,7 @@
+--
+-- Structure de la table `users_admin`
+--
+
 CREATE TABLE IF NOT EXISTS `users_admin` (
   `id` int(11) NOT NULL,
   `name` varchar(150) NOT NULL,
@@ -5,12 +9,19 @@ CREATE TABLE IF NOT EXISTS `users_admin` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `users_admin`
+--
+
 INSERT INTO `users_admin` (`id`, `name`, `forname`) VALUES
 (1, 'Thibault', 'Truffert');
 INSERT INTO `users_admin` (`id`, `name`, `forname`) VALUES
 (2, 'Alex', 'Taurisano');
 
+--
+-- Contraintes pour la table `users_admin`
+--
 ALTER TABLE `users_admin`
   ADD CONSTRAINT `users_admin_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-INSERT INTO `groups` (`name`) VALUES ('administrators');
+
---
+++ memberspace/0.0.3/dbchanges/2_add_confirmed_to_users.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `users` ADD `confirmed` TINYINT NOT NULL DEFAULT '0';
