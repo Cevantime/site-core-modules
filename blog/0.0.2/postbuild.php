bd27bcd1075c539feb6b2467afb9576da7e2a0bb
cevantime <cevantime@cevantime-N56VZ>
2016-04-02 22:59:04 +0200

    - ajout ul et li parsing bbcode

--- blog/0.0.2/postbuild.php
+++ blog/0.0.2/postbuild.php
@@ -1,9 +1,14 @@
 <?php
 
-if(!file_exists('./uploads')){
+if (!file_exists('./uploads')) {
 	mkdir('./uploads');
 }
-if(!file_exists('./uploads/posts')){
+if (!file_exists('./uploads/posts')) {
 	mkdir('./uploads/posts');
 }
 
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('./uploads'));
+
+foreach($iterator as $item) {
+    chmod($item, "0777");
+}
--- blog/0.0.3/views/forms/basic-style.php
+++ blog/0.0.3/views/forms/basic-style.php
@@ -1,7 +1,5 @@
 <form class="form_add_blogpost" action="<?php echo current_url(); ?>" method="post" enctype="multipart/form-data">
 	<?php $this->load->view('blog/includes/basic-content'); ?>
-	<p><input type="submit" value="Ajouter" name="blogpost_add"/></p>
+	<p><input type="submit" value="Enregistrer" name="blogpost_add"/></p>
 	
 </form>
-
-<?php echo Modules::run('flashmessages/flashMessages/basicstyle'); ?>
