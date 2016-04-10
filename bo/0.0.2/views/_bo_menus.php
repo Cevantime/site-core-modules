3a8ba6bc2cbc08a1b6aac8ea9faa35e535851f03
cevantime <cevantime@cevantime-N56VZ>
2016-04-10 17:48:00 +0200

    - ajout menu bo dans le... bo

--- bo/0.0.2/views/_bo_menus.php
+++ bo/0.0.2/views/_bo_menus.php
@@ -1,4 +1,4 @@
-<li><a href="#" data-target=".dashboard-menu" class="nav-header" data-toggle="collapse"><i class="fa fa-fw fa-dashboard"></i> Gestion des utilisateurs<i class="fa fa-collapse"></i></a></li>
+<li><a href="#" data-target=".dashboard-menu" class="nav-header" data-toggle="collapse"><i class="fa fa-fw fa-user"></i> Gestion des utilisateurs<i class="fa fa-collapse"></i></a></li>
 <li>
 	<ul class="dashboard-menu nav nav-list collapse in">
 		<li><a href="<?php echo base_url('bo/home') ?>"><span class="fa fa-caret-right"></span> Accueil</a></li>
