f9f4b3467957ad045be6a5d1ccff3a7bfcedfee5
cevantime <cevantime@cevantime-N56VZ>
2016-04-10 20:15:02 +0200

    - ajout menu bo dans le... bo

--- maintenance/0.0.1/views/edit.php
+++ maintenance/0.0.1/views/edit.php
@@ -2,11 +2,11 @@
 	
 	<?php if($this->configuration->getValue('maintenance')): ?>
 	<p>
-		Le site est actuellement en mode privé. <a class="btn btn-primary" href="<?php echo base_url('maintenance/configure/turnoff'); ?>"><i class="fa fa-ok"></i> Désactiver le mode privé</a>
+		Le site est actuellement en mode privé. <a class="btn btn-primary" href="<?php echo base_url('maintenance/configure/turnoff'); ?>"><i class="fa fa-users"></i> Désactiver le mode privé</a>
 	</p>
 	<?php else: ?>
 	<p>
-		Le site est actuellement en mode public. <a class="btn btn-danger" href="<?php echo base_url('maintenance/configure/turnon'); ?>"><i class="fa fa-users"></i> Activer le mode privé</a>
+		Le site est actuellement en mode public. <a class="btn btn-danger" href="<?php echo base_url('maintenance/configure/turnon'); ?>"><i class="fa fa-lock"></i> Activer le mode privé</a>
 	</p>
 	
 	<?php endif; ?>
