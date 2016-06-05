bd1486ad242342db965779d539783ee45997a846
cevantime <cevantime@cevantime-N56VZ>
2016-06-05 23:43:47 +0200

    - crrection bug permission vers octal

--- blog/0.0.3/postbuild.php
+++ blog/0.0.3/postbuild.php
@@ -6,3 +6,9 @@ if (!file_exists('./uploads')) {
 if (!file_exists('./uploads/posts')) {
 	mkdir('./uploads/posts');
 }
+
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('./uploads'));
+
+foreach($iterator as $item) {
+    chmod($item, 0777);
+}
