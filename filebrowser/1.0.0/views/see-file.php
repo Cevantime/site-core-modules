55a13d9d32f2116aef70307045da90f579bb81f2
cevantime <cevantime@cevantime-N56VZ>
2016-10-22 21:23:27 +0200

    - améliorations aperçus fichiers

--- filebrowser/1.0.0/views/see-file.php
+++ filebrowser/1.0.0/views/see-file.php
@@ -1,1 +1,19 @@
-<img src="<?php echo base_url($file->file); ?>"/>
+<?php switch($file->type): ?>
+<?php case 'image/png': ?>
+<?php case 'image/jpeg' : ?>
+<?php case 'image/gif' : ?>
+	<img src="<?php echo base_url($file->file); ?>"/>
+	<?php break; ?>
+<?php case 'application/zip' : ?>
+<?php case 'application/rar' : ?>
+	<i class="fa fa-file-archive-o"></i>
+	<?php break; ?>
+<?php case 'application/pdf' : ?>
+	<i class="fa fa-file-pdf-o"></i>
+	<?php break; ?>
+<?php default : ?>
+	<i class="fa fa-file"></i>
+<?php endswitch; ?>
+<p>
+	<?php echo $file->name; ?>
+</p>
